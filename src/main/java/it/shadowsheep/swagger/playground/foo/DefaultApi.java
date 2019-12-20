27aa44faa6d2e64cfce7b5f2d4f364ebee12a242
Fabio Bombardi <shadowsheep1@users.noreply.github.com>
2019-12-20 12:31:30 +0100

    Fields must be public

--- src/main/java/it/shadowsheep/swagger/playground/foo/DefaultApi.java
+++ src/main/java/it/shadowsheep/swagger/playground/foo/DefaultApi.java
@@ -1,6 +1,7 @@
 package it.shadowsheep.swagger.playground.foo;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,26 +10,52 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.Serializable;
 
-@Path("/foobar")
+@Path("/v1")
 @Produces({"application/json"})
 public class DefaultApi {
 
     @POST
+    @Path("/foobar")
     @Operation(summary = "Get FooBar",
-            description = "Get FooBar whatever thing it is")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = FooBar.class)
-                    )
-            )})
+            description = "Get FooBar whatever thing it is",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON,
+                                    schema = @Schema(implementation = FooBar.class)
+                            )
+                    )})
     public Response getYourFooBar(
+            @Parameter(schema = @Schema(implementation = FooBar.class))
                     FooBar f
     ) {
         return Response.ok(FooBar.TEDDY_BEAR).build();
     }
 
+    @POST
+    @Path("/foobar2")
+    @Operation(summary = "Get FooBar2",
+            description = "Get FooBar2 whatever thing it is",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON,
+                                    schema = @Schema(implementation = FooBar.class)
+                            )
+                    )})
+    public Response getYourFooBar2(
+            @Parameter(schema = @Schema(implementation = FooBar2.class))
+                    FooBar2 f
+    ) {
+        return Response.ok(FooBar.TEDDY_BEAR).build();
+    }
+
+    public class FooBar2 {
+        public Long userId;
+        public int myInt;
+    }
 }
--- src/main/java/it/shadowsheep/swagger/playground/foo/FooBar.java
+++ src/main/java/it/shadowsheep/swagger/playground/foo/FooBar.java
@@ -1,9 +1,6 @@
 package it.shadowsheep.swagger.playground.foo;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
 //@ExtensionProperty(name = "enum-varnames", value = "ciaone", parseValue = true)
-//@XmlRootElement(name = "FooBar")
 public enum FooBar {
     WINNIE_THE_POO,
     TEDDY_BEAR,
