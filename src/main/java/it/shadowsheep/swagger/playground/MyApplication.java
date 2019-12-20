31597e3fea80f41d7473c11844728e7c581f68b9
Fabio Bombardi <shadowsheep1@users.noreply.github.com>
2019-12-20 09:53:30 +0100

    Add commit

--- src/main/java/it/shadowsheep/swagger/playground/MyApplication.java
+++ src/main/java/it/shadowsheep/swagger/playground/MyApplication.java
@@ -4,5 +4,10 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
 
+/*
+    - https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Integration-and-Configuration
+
+ */
+
 @ApplicationPath("/sample")
 public class MyApplication extends ResourceConfig {}
