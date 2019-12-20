52fd0b9a2503489915a19fc282301a0dd1e8974b
Fabio Bombardi <shadowsheep1@users.noreply.github.com>
2019-12-20 10:51:58 +0100

    Working only with foobar

--- src/main/java/it/shadowsheep/swagger/playground/foo/DefaultApi.java
+++ src/main/java/it/shadowsheep/swagger/playground/foo/DefaultApi.java
@@ -1,22 +1,34 @@
 package it.shadowsheep.swagger.playground.foo;
 
-import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/foobar")
-@Api(tags = {"foobar"})
 @Produces({"application/json"})
 public class DefaultApi {
 
-    @GET
-    @Operation(summary = "Get users",
-            description = "Get list of users")
-    public FooBar getUsers() {
-        return FooBar.WINNIE_THE_POO;
+    @POST
+    @Operation(summary = "Get FooBar",
+            description = "Get FooBar whatever thing it is")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = FooBar.class)
+                    )
+            )})
+    public Response getYourFooBar(
+                    FooBar f
+    ) {
+        return Response.ok(FooBar.TEDDY_BEAR).build();
     }
 
 }
--- src/main/java/it/shadowsheep/swagger/playground/foo/FooBar.java
+++ src/main/java/it/shadowsheep/swagger/playground/foo/FooBar.java
@@ -1,13 +1,12 @@
 package it.shadowsheep.swagger.playground.foo;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
 //@ExtensionProperty(name = "enum-varnames", value = "ciaone", parseValue = true)
+//@XmlRootElement(name = "FooBar")
 public enum FooBar {
     WINNIE_THE_POO,
     TEDDY_BEAR,
-    MASHA
+    MASHA,
+    BEAR
 }
